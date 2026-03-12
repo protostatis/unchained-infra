c74a6754330d721511eb8c5b27ea466e03cd6290
protostatis.dev <protostatis.dev@gmail.com>
2026-03-11 22:13:14 -0500

    Bump packaged agent version for archive restore fix

--- unchained/agent_package.py
+++ unchained/agent_package.py
@@ -14,8 +14,8 @@ import io
 import os
 import zipfile
 
-VERSION = "0.3.45"
-MIN_VERSION = "0.3.45"
+VERSION = "0.3.46"
+MIN_VERSION = "0.3.46"
 
 # Source files to include as-is (non-proprietary)
 _PACKAGE_FILES = {
